fcebab74d3c33252d9350e68261d9339fbab90c9
Sam Alba <sam.alba@gmail.com>
2021-06-17 17:06:11 +0200

    init script to initialize key
    Signed-off-by: Sam Alba <sam.alba@gmail.com>

---
+++ todoapp/init-key.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+KEY_FILE="$HOME/.config/dagger/keys.txt"
+
+# check if dagger is already initialized
+[ -f "$KEY_FILE" ] || {
+    # Any dagger command inits the default key
+    dagger version >/dev/null
+}
+
+# Check if the key has been already set
+grep -q \
+    age1gunf55cd2v30j76w4arxgmzks48v2a56pdw0vtn2j2ax6q2yp3wqgqlzxm \
+    "$KEY_FILE" && {
+        echo "Key already initialized"
+        exit 0
+    }
+
+# Install shared secret key
+cat > "$KEY_FILE" <<- EOF
+# dagger/examples
+# public key: age1gunf55cd2v30j76w4arxgmzks48v2a56pdw0vtn2j2ax6q2yp3wqgqlzxm
+AGE-SECRET-KEY-1E7U8T98V5JTS7CWEWC6CN2A4ZP0H2SE3UTTQV4WZZ8JW26JC23GSEK76PG
+EOF
+
+echo "Key initialized"
